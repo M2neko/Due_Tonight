486e92263b5293f60b7a075fce70450e7d0197a4
JinghanZhang <jhzzhang@ucdavis.edu>
2019-11-21 21:36:07 -0800

    Update

--- Overcodeeeeeeee/Assets/Resource/Script/UI_Script/Next1.cs
+++ Overcodeeeeeeee/Assets/Resource/Script/UI_Script/Next1.cs
@@ -10,6 +10,24 @@ public class Next1 : MonoBehaviour
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
+    public void Next2()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
+
+    public void RandomBG()
+    {
+        switch (Random.Range(1, 3))
+        {
+            case 1:
+               SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+               break;
+            case 2:
+               SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+               break;
+        }
+    }
+
     public void Back()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
