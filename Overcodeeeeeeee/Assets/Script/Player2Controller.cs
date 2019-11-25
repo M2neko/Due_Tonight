6da12405fdd9eadaea9718a9f9b03c25a7c73a9e
jasonz <yizzhou@ucdavis.edu>
2019-11-24 22:53:59 -0800

    fixed facing

--- Overcodeeeeeeee/Assets/Script/Player2Controller.cs
+++ Overcodeeeeeeee/Assets/Script/Player2Controller.cs
@@ -5,18 +5,37 @@ using UnityEngine;
 public class Player2Controller : MonoBehaviour
 {
     [SerializeField] private float Speed;
+    [SerializeField] private GameObject player1_1;
+    [SerializeField] private GameObject player1_2;
+    private GameObject player1;
     private Vector3 Change;
     private Rigidbody2D PlayerRigidbody;
     private Animator PlayerAnimator;
 
     private void Start()
     {
+        if (player1_1.activeInHierarchy)
+        {
+            player1 = player1_1;
+        }
+        else
+        {
+            player1 = player1_2;
+        }
         PlayerAnimator = this.GetComponent<Animator>();
         PlayerRigidbody = this.GetComponent<Rigidbody2D>();
     }
 
     private void Update()
     {
+        if (this.gameObject.transform.position.x >= player1.transform.position.x)
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().flipX = true;
+        }
+        else
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().flipX = false;
+        }
         if (Input.GetButtonDown("Player2Jump") && this.gameObject.transform.position.y <= -3.32f) 
         {
             PlayerRigidbody.AddForce(new Vector2(0f, 5000f));
--- Overcodeeeeeeee/Assets/Script/PlayerController.cs
+++ Overcodeeeeeeee/Assets/Script/PlayerController.cs
@@ -5,18 +5,37 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float Speed;
+    [SerializeField] private GameObject player2_1;
+    [SerializeField] private GameObject player2_2;
+    private GameObject player2;
     private Vector3 Change;
     private Rigidbody2D PlayerRigidbody;
     private Animator PlayerAnimator;
 
     private void Start()
     {
+        if (player2_1.activeInHierarchy)
+        {
+            player2 = player2_1;
+        }
+        else
+        {
+            player2 = player2_2;
+        }
         PlayerAnimator = this.GetComponent<Animator>();
         PlayerRigidbody = this.GetComponent<Rigidbody2D>();
     }
 
     private void Update()
     {
+        if (this.gameObject.transform.position.x >= player2.transform.position.x)
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().flipX = true;
+        }
+        else
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().flipX = false;
+        }
         if (Input.GetButtonDown("Player1Jump") && this.gameObject.transform.position.y <= -3.32f)
         {
             PlayerRigidbody.AddForce(new Vector2(0f, 5000f));
