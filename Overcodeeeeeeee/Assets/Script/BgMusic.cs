2b456499d855854510b8a907dfe58c247b4ebf95
JinghanZhang <jhzzhang@ucdavis.edu>
2019-12-04 16:10:59 -0800

    + BgMusic

---
+++ Overcodeeeeeeee/Assets/Script/BgMusic.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BgMusic : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    private static BgMusic instance = null;
+    public static BgMusic Instance
+    {
+        get { return instance; }
+    }
+
+    void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        else
+        {
+            instance = this;
+        }
+        DontDestroyOnLoad(this.gameObject);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
