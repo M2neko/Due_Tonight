bc8f984732dac15eb4e92853a225d03271c764d4
JinghanZhang <jhzzhang@ucdavis.edu>
2019-12-06 23:37:18 -0800

    HealthBar

---
+++ Overcodeeeeeeee/Assets/Script/HealthbarController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthbarController : MonoBehaviour
+{
+	/*
+     *DOTween : http://dotween.demigiant.com/getstarted.php
+     *DOTween.To(()=> transform.position, x=> transform.position = x, new Vector3(2,2,2), 1);
+     *对任意数值做动画 可以替换为血条width
+     *DOTween.To(() => GetComponent<RectTransform>().sizeDelta, x => GetComponent<RectTransform>().sizeDelta = x, new Vector2(200, 30), 1);
+     */
+
+	// Start is called before the first frame update
+	void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
