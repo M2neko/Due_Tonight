b7ee08e4f01b76635b8f38d8f4bbd830a3f03eb1
jasonz <yizzhou@ucdavis.edu>
2019-12-08 11:22:53 -0800

    speed punch done

--- Overcodeeeeeeee/Assets/Script/Mccoy_Motions/SpeedPunch.cs
+++ Overcodeeeeeeee/Assets/Script/Mccoy_Motions/SpeedPunch.cs
@@ -6,10 +6,12 @@ public class SpeedPunch : MonoBehaviour
 {
     [SerializeField] private GameObject Mccoy;
     [SerializeField] private float Speed;
+    [SerializeField] private float RushSpeed;
     private Vector3 pos;
     private GameObject OtherPlayer;
     private float check;
     private bool left;
+    private bool punch = false;
     public static bool IsRush = false;
 
     public void Rush()
@@ -18,11 +20,13 @@ public class SpeedPunch : MonoBehaviour
         {
             return;
         }
+        punch = false;
         IsRush = true;
         OtherPlayer = Mccoy.GetComponent<PlayerController>().OtherPlayer();
         pos = Mccoy.gameObject.transform.position;
         check = 0.0f;
-        left |= OtherPlayer.gameObject.transform.position.x >= Mccoy.gameObject.transform.position.x;
+        left |= OtherPlayer.gameObject.transform.position.x <= Mccoy.gameObject.transform.position.x;
+        Mccoy.gameObject.GetComponent<Animator>().SetBool("Punch", true);
     }
 
     private void Update()
@@ -32,48 +36,36 @@ public class SpeedPunch : MonoBehaviour
             check += Time.deltaTime;
             if (!left)
             {
-                if(check >= 0.001 && check <= 0.01)
-                {
-                    Mccoy.GetComponent<Animator>().SetBool("Punch", true);
-                }
                 if (check <= 0.5f)
                 {
-                    Mccoy.gameObject.transform.position = new Vector3(pos.x + Speed, pos.y, pos.z);
+                    Mccoy.gameObject.transform.position = new Vector3(pos.x + Speed * 0.5f, pos.y, pos.z);
                 }
-                else if (check >= 0.5f && check <= 1.5f)
+                else if (check >= 0.5f && !punch)
                 {
-                    Mccoy.gameObject.transform.position = new Vector3(pos.x + Speed * 2.0f, pos.y, pos.z);
+                    punch = true;
+                    Mccoy.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.right * RushSpeed);
                 }
-                else if (check >= 1.5f && check < 1.7f)
-                {
-                    Mccoy.gameObject.transform.position = new Vector3(pos.x + Speed * 1.5f, pos.y, pos.z);
-                }
-                else
+                else if (check >= 1.7f)
                 {
                     IsRush = false;
                     check = 0.0f;
+                    Mccoy.GetComponent<Animator>().SetBool("Punch", false);
                 }
             }
             else if (left)
             {
-                if (check >= 0.001 && check <= 0.01)
-                {
-                    Mccoy.GetComponent<Animator>().SetBool("Punch", true);
-                }
                 if (check <= 0.5f)
                 {
-                    Mccoy.gameObject.transform.position = new Vector3(pos.x - Speed, pos.y, pos.z);
+                    Mccoy.gameObject.transform.position = new Vector3(pos.x - Speed * 0.5f, pos.y, pos.z);
                 }
-                else if (check >= 0.5f && check <= 1.5f)
+                else if (check >= 0.5f && !punch)
                 {
-                    Mccoy.gameObject.transform.position = new Vector3(pos.x - Speed * 2.0f, pos.y, pos.z);
+                    punch = true;
+                    Mccoy.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.left * RushSpeed);
                 }
-                else if (check >= 1.5f && check < 1.7f)
+                else if (check >= 1.7f)
                 {
-                    Mccoy.gameObject.transform.position = new Vector3(pos.x - Speed * 1.5f, pos.y, pos.z);
-                }
-                else
-                {
+                    punch = false;
                     IsRush = false;
                     check = 0.0f;
                     Mccoy.GetComponent<Animator>().SetBool("Punch", false);
